eb60d5bd2d13a8b3b9566c58a8a9dbb0678b408c
SebastianWood <54947163+SebastianWood@users.noreply.github.com>
2019-10-18 18:11:32 +0200

    Add project files.

---
+++ FightSim/Fighter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Fighter
+    {
+
+        public string namn = "";
+
+        public int liv = 10;
+
+
+
+
+
+        
+
+        
+
+        public bool IsAlive = true;
+
+        public int GetHp;
+
+
+
+
+    }
+
+  /*  public static void Attack()
+    {
+            int Attack;
+} */
+
+
+}
---
+++ FightSim/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in namn på fighter: ");
+
+            Fighter fighter = new Fighter();
+            {
+                fighter.namn = Console.ReadLine();
+
+            };
+
+            Console.WriteLine("" + fighter.namn + "!");
+
+            Console.WriteLine("Skriv in namn på fighter2: ");
+
+            Fighter fighter2 = new Fighter();
+            {
+                fighter2.namn = Console.ReadLine();
+
+            };
+
+            
+            Console.WriteLine("" + fighter2.namn + "!");
+
+            Console.ReadLine();
+
+            
+
+
+
+
+            
+
+
+            Console.WriteLine("Nu börjar fighten");
+
+            Console.ReadLine();
+
+
+            Random generator = new Random();
+
+
+           
+
+
+
+            fighter.liv = 9;
+
+            fighter2.liv = 10;
+
+            
+
+            while (fighter2.liv >= 0 && fighter.liv >= 0)
+            {
+
+
+                int damage = generator.Next(0, 5);
+
+                fighter2.liv -= damage;
+
+                
+                Console.WriteLine(""+ fighter.namn + " slår " + fighter2.namn + " med " + damage + "!" );
+                Console.WriteLine("" + fighter2.namn + " har " + fighter2.liv + " liv kvar.");
+
+                Console.ReadLine();
+
+                damage = generator.Next(0, 5);
+
+                fighter.liv -= damage;
+
+                Console.WriteLine("" + fighter2.namn + " slår " + fighter.namn + " med " + damage + "!");
+                Console.WriteLine("" + fighter.namn + " har " + fighter.liv + " liv kvar");
+
+                fighter.liv -= damage;
+
+                Console.ReadLine();
+
+
+            }
+
+
+
+            if (fighter2.liv < 0)
+            {
+
+
+
+                Console.WriteLine("Ser ut som att " + fighter.namn + " vann!! Grattis!");
+
+                Console.ReadLine();
+
+
+
+            }
+
+            else if (fighter.liv < 0)
+            {
+                Console.WriteLine("Ser ut som att " + fighter2.namn + " vann!! Grattis!");
+
+                Console.ReadLine();
+
+
+
+            }
+
+
+
+            Console.WriteLine("Match över!");
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
